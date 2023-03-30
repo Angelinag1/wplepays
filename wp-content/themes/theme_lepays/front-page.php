9b762b41b15ecc7f7a953a0f4e9846ef91c5d614
Angelinag1 <angelina.garaebiti2000@gmail.com>
2023-03-31 09:37:52 +1100

    Update front-page.php

--- wp-content/themes/theme_lepays/front-page.php
+++ wp-content/themes/theme_lepays/front-page.php
@@ -3,6 +3,11 @@
 <div class="container pt-5 pb-5">
 
     <h1><?php the_title(  );?></h1>
+    <?php if (have_posts(  )) : while (have_posts(  )):the_post(  );?>
+
+    <?php the_content(  );?>
+
+<?php endwhile; endif;?>
     <div class="jumbotron" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_posts()[0]->ID); ?>');background-repeat: no-repeat; background-size:cover">
   <?php
     $args = array(
